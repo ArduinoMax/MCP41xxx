2e831e254d1c9deb91b69e741f3c28dd5f25f836
Max Chan <xcvista@me.com>
2016-08-17 23:15:43 +0800

    Code finished. Yet to test.

---
+++ src/MCP41xxx.cpp
@@ -0,0 +1,61 @@
+/*
+ MCP41xxx.cpp
+ MCP41xxx driver for Arduino
+ 
+ Copyright 2015-2016 Max Chan <max@maxchan.info>
+ */
+
+#include "MCP41xxx.h"
+#include <SPI.h>
+
+MCP41xxx::MCP41xxx(uint8_t ss)
+{
+    _ss = ss;
+}
+
+void MCP41xxx::begin(void)
+{
+    // Prepare SPI hardware.
+    SPI.begin();
+    
+    // Prepare SPI SS line
+    digitalWrite(_ss, 1);
+    pinMode(_ss, OUTPUT);
+}
+
+void MCP41xxx::analogWrite(uint8_t gate, uint16_t value)
+{
+
+	// Prepare SPI data;
+	if (value == ADC_SHUTDOWN)
+	{
+		value = 0x2000;
+	}
+	else
+	{
+		value &= 0xff;
+		value |= 0x1000;
+	}
+
+	value |= (gate & 0x1) ? 0x200 : 0x100;
+    
+    // Assert SPI bus
+    digitalWrite(_ss, 0);
+    
+    // Perform SPI Transfer
+    SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
+
+	uint8_t zeros = gate >> 1;
+	for (uint8_t idx = 0; idx < zeros; idx++)
+	{
+		// Send zeros to bypass irelevant chips
+		SPI.transfer16(0);
+	}
+	SPI.transfer16(value);
+
+    SPI.endTransaction();
+    
+    // Deassert SPI bus
+    digitalWrite(_ss, 1);
+}
+
---
+++ src/MCP41xxx.h
@@ -0,0 +1,43 @@
+/*
+ MCP41xxx.h
+ MCP41xxx driver for Arduino
+ 
+ Copyright 2015-2016 Max Chan <max@maxchan.info>
+ */
+
+#ifndef _MCP41XXX_H_
+#define _MCP41XXX_H_
+
+#include <Arduino.h>
+
+#define ADC_SHUTDOWN UINT16_MAX
+
+/// Driver for Microchip MCP41xxx digi-pot.
+class MCP41xxx
+{
+private:
+    uint8_t _ss;
+    
+public:
+    /*!
+     Initialize the TLC5615 driver.
+     
+     @param ss GPIO pin used for the chip select for the DAC.
+     */
+    MCP41xxx(uint8_t ss);
+    
+    /*!
+     Initialize the SPI bus and the DAC chip.
+     */
+    void begin(void);
+    
+    /*!
+     Write a value to the DAC chip.
+     
+     @param value The DAC value. It is capped to 10 bits and excessive bits are
+     ignored.
+     */
+    void analogWrite(uint8_t gate = 0, uint16_t value);
+};
+
+#endif // !defined(_TLC5615_H_)
